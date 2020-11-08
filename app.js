3d5e6108a79062cf3f6227f0b16294f898c11033
Joe Powell <joe_773@yahoo.com>
2020-11-08 10:46:25 -0600

    mesage

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const PORT = process.env.PORT || 3000;
 const express = require('express')
 const app = express();
 const mongoose = require('mongoose');
@@ -177,7 +178,7 @@ app.patch('/updatePost', async (req, res) => {
 
 
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("server started on port 3000");
 
 })
